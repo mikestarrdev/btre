714747294e3b1acb989559fb7c8b447a69f17768
Mike Starr <imhs@protonmail.com>
2024-02-04 11:52:44 -0500

    postgres init

---
+++ pages/apps.py
@@ -0,0 +1,8 @@
+"""Module providing a function printing python version."""
+
+from django.apps import AppConfig
+
+
+class PagesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pages'
---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+"""Module providing a function printing python version."""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('', views.index, name='index'),
+  path('about/', views.about, name='about'),
+]
